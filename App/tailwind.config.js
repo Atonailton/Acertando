1903ff4fbd958acad6c5ddf914d1f439cd762b3e
Atonailton <atonds@gmail.com>
2025-04-18 21:01:28 -0300

    Create tailwind.config.js

---
+++ App/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Direcionando para os arquivos em app
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
